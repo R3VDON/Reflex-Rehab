29baf1370bcc94088306d1c2e2712593f283cdff
Konrad Fornal <revdon@windowslive.com>
2024-10-27 22:00:07 +0100

    Dodanie kontrolki użytkownika obsługującej łatwy poziom trudności gry

---
+++ Reflex Rehab/Poziom_Latwy.Designer.cs
@@ -0,0 +1,40 @@
+﻿namespace Reflex_Rehab {
+    partial class Poziom_Latwy {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent() {
+            this.SuspendLayout();
+            // 
+            // Poziom_Latwy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Poziom_Latwy";
+            this.Size = new System.Drawing.Size(1280, 1024);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
