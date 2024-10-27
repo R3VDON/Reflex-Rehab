af425f7937847faa4d9bc0c086a4932414668f23
Konrad Fornal <revdon@windowslive.com>
2024-10-27 16:24:35 +0100

    Zmiana wyglądu menu głównego oraz dodanie nowych przycisków

--- Reflex Rehab/Menu_Glowne.cs
+++ Reflex Rehab/Menu_Glowne.cs
@@ -8,25 +8,32 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Reflex_Rehab
-{
-    public partial class Menu_Glowne : Form
-    {
-        public Menu_Glowne()
-        {
+namespace Reflex_Rehab{
+    public partial class Menu_Glowne : Form{
+        private Button currentBtn;
+        private Button previousBtn;
+
+        public Menu_Glowne(){
             InitializeComponent();
         }
 
-        private void Menu_Glowne_Load(object sender, EventArgs e)
-        {
+        private void Menu_Glowne_Load(object sender, EventArgs e){
 
         }
 
-        private void Button1_Click(object sender, EventArgs e)
-        {
+        //private void Btn_Sellect(object btnSender){
+            //if (btnSender == null) { 
+            
+            //}
+        //}
+
+        private void BtnStartGame_Click(object sender, EventArgs e){
             this.Hide();
             Poziom_Trudnosci poziom_Trudnosci = new Poziom_Trudnosci();
             poziom_Trudnosci.Show();
+        }
+        private void BtnExit_Click(object sender, EventArgs e){
+            Application.Exit();
         }
     }
 }
